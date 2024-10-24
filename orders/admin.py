d4eb6ed8ff7abb55d306c941a3d896f5c0fa3490
MRamadan2511 <m.ramadan2511@gmail.com>
2024-10-24 21:36:40 +0300

    create order model

--- orders/admin.py
+++ orders/admin.py
@@ -1,2 +1,6 @@
 from django.contrib import admin
 
+from .models import OrderStatus, Order, OrderItem
+
+admin.site.register(OrderStatus)
+
--- orders/models.py
+++ orders/models.py
@@ -1,9 +1,8 @@
 from django.db import models
 from accounts.models import Customer
-from products.models import Product, ProductProfile
+from products.models import ProductProfile
 from django.db.models import Sum
 from django.utils import timezone
-
 
 
 class OrderStatus(models.Model):
@@ -13,27 +12,25 @@ class OrderStatus(models.Model):
         return self.status
 
 
-from django.db import models
-from accounts.models import User
-from products.models import ProductProfile
-
 class Order(models.Model):
-    customer = models.ForeignKey(User, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     order_date = models.DateTimeField(auto_now_add=True)
-    total_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
     is_paid = models.BooleanField(default=False)
     payment_method = models.CharField(max_length=50, blank=True, null=True)
-    delivery_address = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=20, default="New")
 
     def __str__(self):
         return f"Order #{self.id} by {self.customer.email}"
 
-    def calculate_total(self):
-        """
-        Calculate the total amount of the order based on the cart items and their prices.
-        """
-        total = sum(item.get_subtotal() for item in self.cart_items.all())
-        self.total_amount = total
-        self.save()
-        return total
 
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
+    product = models.ForeignKey(ProductProfile, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def get_cost(self):
+        return self.price * self.quantity
